242e5a496b0974f483afcc514750d3257764fe13
Paul Haddad <paul.haddad@yello.co>
2017-05-01 12:21:52 -0500

    Get failing test for dash redirect

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 feature "User signs up" do
   scenario "using valid credentials" do
     visit new_user_registration_path
-    fill_in "Email", with: "test@example.com"
-    fill_in "Password", with: "password"
+    fill_in "user_email", with: "test@example.com"
+    fill_in "user_password", with: "password"
     click_button "Sign up"
 
-    expect(current_path).to eq(root_path)
+    expect(current_path).to eq(dashboard_path)
   end
 end
