1f8bbb27cb2c9d60e828767e05efea23c866c96f
Paul Haddad <paul.haddad@yello.co>
2017-05-01 12:42:25 -0500

    Allow signing out

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -4,8 +4,8 @@ feature "User signs up" do
   scenario "using valid credentials" do
     visit new_user_registration_path
     fill_in "user_email", with: "test@example.com"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "user_password", with: "abcdef"
+    fill_in "user_password_confirmation", with: "abcdef"
     click_button "Sign up"
 
     expect(current_path).to eq(dashboard_path)
