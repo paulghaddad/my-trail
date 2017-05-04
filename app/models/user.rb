aff215e1be5dbdb073e9a07ba165442035727616
Paul Haddad <paul.haddad@yello.co>
2017-05-04 14:58:03 -0500

    Create new entry when email is received

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :entries
+
+  def newest_entry
+    entries.last
+  end
 end
